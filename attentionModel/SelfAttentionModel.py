5b85a2afc58c781fabb765536042fc7e4e559f38
Alžbeta Štajerová <bets.stajerova@gmail.com>
2019-04-14 23:56:51 +0200

    self attentive model refactoring - add elmo bert embedding

--- attentionModel/SelfAttentionModel.py
+++ attentionModel/SelfAttentionModel.py
@@ -1,50 +1,100 @@
 # model based on Structured Self-attentive Sentence Embedding - https://arxiv.org/pdf/1703.03130.pdf
 import json
+import sys
 
+from allennlp import training
+from allennlp.commands.elmo import ElmoEmbedder, batch_to_ids
+from allennlp.modules import TextFieldEmbedder, Elmo
+from allennlp.modules.token_embedders import Embedding
+from pytorch_pretrained_bert.modeling import BertConfig, BertModel, BertEmbeddings
 import torch
 import torch.nn as nn
+
+sys.path.append("..")
+from utils.utils import ids2str
 
 
 class SelfAttentionModel(nn.Module):
     with open('config.json', 'r') as f:
         config = json.load(f)
 
-    def __init__(self, vocabulary, num_classes, device, u=config['hidden_units'], da=config['d_a'], r=config['extraction_count']):
+    def __init__(self, vocabulary, num_classes, device, u=config['hidden_units'], da=config['d_a'],
+                 r=config['extraction_count'], pad_masking=config['pad_masking'], embeddings=config['embeddings'],
+                 hidden_classifier_units=config['hidden_classifier_units'],
+                 classifier_dropout=config['classifier_dropout']):
         super(SelfAttentionModel, self).__init__()
-        self.vocab = vocabulary
-        self.embeddings_dim = vocabulary.vectors.shape[1]
-        self.embeddings = nn.Embedding(len(vocabulary), self.embeddings_dim)
-        self.vocab.vectors[self.vocab.stoi['<pad>']] = -1e8 * self.embeddings_dim  # pad token masking
-        self.embeddings.weight.data.copy_(vocabulary.vectors.to(device))
-
+        self.device = device
         self.u = u  # hidden unit for each unidirectional LSTM
         self.r = r  # number of different parts to be extracted from the sentence.
+
+        self.embeddings = embeddings
+        self.embeddings_dim = 300
+        self.vocab_size = len(vocabulary.stoi)
+        self.vocab = vocabulary
+        if self.embeddings == 'bert':
+            self.bert_config = BertConfig(vocab_size_or_config_json_file=self.vocab_size, hidden_size=768,
+                                          num_hidden_layers=12, num_attention_heads=12, intermediate_size=3072)
+            self.bert = BertModel(self.bert_config).from_pretrained('bert-base-uncased')
+            self.embeddings_dim = self.bert.embeddings.word_embeddings.embedding_dim
+        elif self.embeddings == 'elmo':
+            # self.elmo_embedding = Embedding(num_embeddings=self.vocab_size, embedding_dim=self.embeddings_dim)
+            options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
+            weight_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
+            self.elmo_embedder = ElmoEmbedder(options_file, weight_file)
+            self.elmo = Elmo(options_file, weight_file, 1)
+            self.embeddings_dim = 1024
+        elif self.embeddings == 'glove':
+            self.embeddings_dim = vocabulary.vectors.shape[1]
+            self.embeddings = nn.Embedding(len(vocabulary), self.embeddings_dim)
+            if pad_masking:
+                self.vocab.vectors[self.vocab.stoi['<pad>']] = -1e8 * self.embeddings_dim  # pad token masking
+            self.embeddings.weight.data.copy_(vocabulary.vectors.to(device))
 
         self.bilstm = nn.LSTM(input_size=self.embeddings_dim, hidden_size=u, num_layers=1, bidirectional=True)  # batch_first=True
         self.Ws1 = nn.Linear(2 * u, da, bias=False)
         self.Ws2 = nn.Linear(da, r, bias=False)
-        # self.lin3 = nn.Linear(r, num_classes)
-        self.lin3 = nn.Linear(r * 2 * u, num_classes)
+        self.lin3 = nn.Linear(r * 2 * u, hidden_classifier_units)  # MLP
+        self.pred = nn.Linear(hidden_classifier_units, num_classes)
         self.softmax = nn.Softmax(dim=1)
+        self.dropout = torch.nn.Dropout(classifier_dropout)
+        self.relu = torch.nn.ReLU()
         # softmax is included in CrossEntropyLoss
 
+    # TODO weight init
+
     def self_attention(self, H):
-        out_ws1 = self.Ws1(H)           # Ws1^T * H
+        out_ws1 = self.Ws1(H)               # Ws1^T * H
         out_tahn = torch.tanh(out_ws1)      # tahn (Ws1^T * Ht)
-        out_ws2 = self.Ws2(out_tahn)    # tahn (Ws1 * Ht) * Ws2^T
-        out_a = self.softmax(out_ws2)      # softmax( Ws2 * tahn (Ws1 * Ht) )
+        out_ws2 = self.Ws2(out_tahn)        # tahn (Ws1 * Ht) * Ws2^T
+        out_a = self.softmax(out_ws2)       # softmax( Ws2 * tahn (Ws1 * Ht) )
         return out_a
 
+    def get_embeddings(self, w):
+        if self.embeddings == 'bert':
+            embedding_output = self.bert.embeddings(w)
+        elif self.embeddings == 'elmo':
+            character_ids = batch_to_ids(ids2str(w, self.vocab))
+            embeddings = self.elmo(character_ids.to(device=self.device))
+            embedding_output = embeddings['elmo_representations'][0].to(self.device)
+        else:
+            embedding_output = self.embeddings(w)
+        return embedding_output
+
     def forward(self, w):
-        out = self.embeddings(w)
+        embedding_output = self.get_embeddings(w)
+
         # bidirectional pass of LSTM - H
-        out_lstm, _ = self.bilstm(out)
+        out_lstm, _ = self.bilstm(embedding_output)
         # self-attention mechanism - A
         out_a = self.self_attention(out_lstm)
 
         out_a = out_a.transpose(2, 1)
         out_m = torch.bmm(out_a, out_lstm)  # M = A * H - visualization
         out = out_m.view(out_m.shape[0], self.r * 2 * self.u)
-        out = self.lin3(out)
 
-        return out, out_a
+        # MLP
+        # out = self.lin3(self.relu(self.dropout(out)))
+        out = self.relu(self.lin3(self.dropout(out)))
+        pred = self.pred(self.dropout(out))
+
+        return pred, out_a
--- attentionModel/attention.py
+++ attentionModel/attention.py
@@ -1,17 +1,29 @@
 import json
 import matplotlib
 import matplotlib.pyplot as plt
+import sys
 
+from allennlp.data import Vocabulary, Tokenizer, DatasetReader
+from allennlp.data.fields import TextField
+from allennlp.data.iterators import BucketIterator
+from allennlp.data.token_indexers import ELMoTokenCharactersIndexer
+from allennlp.modules import TextFieldEmbedder
+from nltk.tokenize.punkt import PunktSentenceTokenizer
 import numpy as np
+from pytorch_pretrained_bert import BertTokenizer
 from sklearn.metrics import f1_score
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim
 from torchtext import data as torchdata
 from torchtext.vocab import GloVe
 from tqdm import tqdm
 
 from SelfAttentionModel import SelfAttentionModel
+
+sys.path.append("..")
+from utils import utils
 
 
 def get_weights(labels):
@@ -43,7 +55,7 @@ def ids2text(batch, vocab):
 # visualize all extractions attentions for one document
 def visualize_doc_all_extractions(doc_attention, doc_text, ):
     fig, ax = plt.subplots()
-    im = ax.imshow(doc_attention.cpu().detach().numpy(), cmap='YlGn')
+    im = ax.imshow(doc_attention.cpu().detach().numpy(), cmap='Blues')
 
     ax.set_xticks(np.arange(len(doc_text)))
     ax.set_yticks(np.arange(doc_attention.shape[0]))
@@ -59,12 +71,11 @@ def visualize_doc_all_extractions(doc_attention, doc_text, ):
 
 # visualize all joined attention extractions for batch documents
 def visualize_joined_att(attention, docs_text):
-    # join extraction
     sum_att = torch.sum(attention, dim=1)  # sum over annotations
     norm_att = F.softmax(sum_att, dim=1)  # normalize to sum up to one
     for doc_att, doc_text in zip(norm_att, docs_text):
         fig, ax = plt.subplots()
-        im = ax.imshow([doc_att.cpu().detach().numpy()])
+        im = ax.imshow([doc_att.cpu().detach().numpy()], cmap='Blues')
         ax.set_xticks(np.arange(len(doc_text)))
         ax.set_xticklabels(doc_text)
 
@@ -83,19 +94,18 @@ def visualize(batch, attention, vocab):
     if config['visualize_all_extractions']:
         for doc_attention, doc_text in zip(attention, docs_text):
             visualize_doc_all_extractions(doc_attention, doc_text)
-    else:
+    if config['visualize_joined_extractions']:
         visualize_joined_att(attention, docs_text)
 
 
 def frobenius_norm(p):
-    pen_loss = torch.sum(p ** 2) ** 0.5  # sum(p^2)^0.5
-    return pen_loss
+    pen_loss = (torch.sum(torch.sum((p ** 2), 1), 1).squeeze()) ** 0.5
+    return torch.sum(pen_loss) / p.shape[0]
 
 
 def train(model, criterion, optimiser, train_iterator, vocab, device):
     model.train()
 
-    train_loss = 0
     total_correct = 0
     total_batches = len(train_iterator.data()) // train_iterator.batch_size
     model_predictions = []
@@ -103,6 +113,9 @@ def train(model, criterion, optimiser, train_iterator, vocab, device):
 
     for epoch in range(config['num_epochs']):
         pbar = tqdm(total=total_batches)
+        epoch_correct = 0
+        train_loss = 0
+        epoch_predictions = 0
         for i, batch in enumerate(train_iterator):
             predictions, attention = model(batch.text)  # forward pass
             # visualize(batch.text, attention, vocab)
@@ -112,9 +125,10 @@ def train(model, criterion, optimiser, train_iterator, vocab, device):
             # penalization term
             if config['penalization_form']:
                 # A * A^T - identity matrix
+                size = attention.shape
                 pen = torch.bmm(attention, attention.transpose(2, 1))
                 pen -= torch.eye(config['extraction_count'], requires_grad=False, device=device)\
-                    .expand(config['batch_size'], config['extraction_count'], config['extraction_count'])
+                    .expand(size[0], config['extraction_count'], config['extraction_count'])
                 pen_loss = frobenius_norm(pen)  # frob_norm(penalization)
                 loss += pen_loss * config['coefficient']
 
@@ -124,12 +138,15 @@ def train(model, criterion, optimiser, train_iterator, vocab, device):
             true_labels = true_labels + batch.label.cpu().detach().tolist()
             model_predictions = model_predictions + label_pred
             for p, tp in zip(label_pred, batch.label.cpu().detach().tolist()):
+                epoch_predictions += 1
                 if p == tp:
                     total_correct += 1
+                    epoch_correct += 1
 
             pbar.set_description(
-                f'Loss: {train_loss / ((i + 1) * (epoch + 1)):.7f} ' +
-                f'Acc: {total_correct / ((len(batch) * (i + 1)) * (epoch + 1)):.7f} ' +
+                f'{epoch + 1}/{config["num_epochs"]} ' +
+                f'Loss: {train_loss / (i + 1):.7f} ' +
+                f'Acc: {epoch_correct / epoch_predictions:.7f} ' +
                 f'F1: {f1_score(true_labels, model_predictions, average="macro"):.7f} ' +
                 f'Total correct {total_correct} out of {len(model_predictions)}\n'
             )
@@ -147,20 +164,23 @@ def test(model, test_iterator, vocab):
 
     total_correct = 0
     total_batches = len(test_iterator.data()) // test_iterator.batch_size
-    pbar = tqdm(total=total_batches)
-    for i, batch in enumerate(train_iterator):
+    true_labels = []
+    model_predictions = []
+    # pbar = tqdm(total=total_batches)
+    for i, batch in enumerate(test_iterator):
         predictions, attention = model(batch.text)  # forward pass
-        visualize(batch.text, attention, vocab)
+        if config['visualization']:
+            visualize(batch.text, attention, vocab)
         label_pred = [np.argmax(p) for p in predictions.cpu().detach().numpy()]
         true_labels = true_labels + batch.label.cpu().detach().tolist()
         model_predictions = model_predictions + label_pred
         for p, tp in zip(label_pred, batch.label.cpu().detach().tolist()):
             if p == tp:
                 total_correct += 1
-        pbar.update(1)
+        # pbar.update(1)
 
     print(
-        f'Acc: {total_correct / (len(batch) * (i + 1)):.7f} ' +
+        f'\n\n\nAcc: {total_correct / len(model_predictions):.7f} ' +
         f'F1: {f1_score(true_labels, model_predictions, average="macro"):.7f} ' +
         f'Total correct {total_correct} out of {len(model_predictions)}\n'
     )
@@ -170,18 +190,46 @@ def save_model(model):
     torch.save(model.state_dict(), 'modelSelfAtt.ckpt')
 
 
+bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+sentence_tokenizer = PunktSentenceTokenizer()
+
+
+def bert_tokenize(text):
+    sentences = sentence_tokenizer.tokenize(text)
+    text = ''
+    for sentence in sentences:
+        text += sentence + ' [SEP] '
+    tokens = []
+    tokens.append("[CLS]")
+    tokens += bert_tokenizer.tokenize(text)
+    return tokens
+
+
 if __name__ == '__main__':
     with open('config.json', 'r') as f:
         config = json.load(f)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    TEXT = torchdata.Field(tokenize="spacy", sequential=True, lower=True, batch_first=True)
+    # if config['embeddings'] == 'elmo':
+    #     token_indexer = ELMoTokenCharactersIndexer()
+    #     ds_reader = DatasetReader()
+    #     train_ds, test_ds = (ds_reader.read(dataset) for dataset in [config['dataset_train'], config['dataset_test']])
+    #     vocab = Vocabulary()
+    #     train_iterator = BucketIterator(train_ds, batch_size=config['batch_size'])
+    #     test_iterator = BucketIterator(test_ds, batch_size=config['batch_size'])
+
+    # else:
+    text_tokenizer = bert_tokenize if config["embeddings"] == "bert" else "spacy"
+    TEXT = torchdata.Field(tokenize=text_tokenizer, sequential=True, lower=True, batch_first=True)
+
     LABEL = torchdata.Field(use_vocab=False, sequential=False, preprocessing=lambda x: int(x), is_target=True)
 
-    tabular_dataset = torchdata.TabularDataset(config['dataset'], format='tsv',
-                                               fields=[('label', LABEL), ('text', TEXT)])
-    train_dataset, test_dataset = tabular_dataset.split()
+    train_dataset, test_dataset = torchdata.TabularDataset.splits(path=config['dataset_path'],
+                                                                  train=config['dataset_train'],
+                                                                  test=config['dataset_test'],
+                                                                  format='tsv',
+                                                                  fields=[('label', LABEL), ('text', TEXT)])
 
     train_iterator = torchdata.BucketIterator(train_dataset, batch_size=config['batch_size'],
                                               sort_key=lambda x: len(x.text),
@@ -191,21 +239,31 @@ if __name__ == '__main__':
                                              sort_key=lambda x: len(x.text),
                                              device=device,
                                              sort_within_batch=False)
+
+    # vocab = Vocabulary.from_instances(train_dataset + test_dataset)
+
+    LABEL.build_vocab(train_dataset)
     if config['embeddings'] == 'glove':
-        TEXT.build_vocab(tabular_dataset, vectors=GloVe(name='6B', dim=300))
+        TEXT.build_vocab(train_dataset, vectors=GloVe(name='6B', dim=300))
+    elif config['embeddings'] == 'bert' or config['embeddings'] == 'elmo':
+        TEXT.build_vocab(train_dataset)
     else:
-        raise NotImplementedError('Embeddings should be either "glove"')
+        raise NotImplementedError('Embeddings should be either "glove", "bert", "elmo".')
 
-    LABEL.build_vocab(tabular_dataset)
-
-    num_classes, weights = get_weights([e.label for e in tabular_dataset.examples])
+    num_classes, weights = get_weights([e.label for e in train_dataset.examples])
 
     selfAttModel = SelfAttentionModel(vocabulary=TEXT.vocab, device=device, num_classes=num_classes).to(device)
+    if config['load_checkpoint']:
+        selfAttModel.load_state_dict(torch.load(config['checkpoint'], map_location=device))
+
+    print(f'Model has {utils.count_parameters(selfAttModel)} trainable parameters')
+
+    optimiser = torch.optim.Adam(selfAttModel.parameters(), lr=config['learning_rate'], weight_decay=0.0001)
     criterion = nn.CrossEntropyLoss(weight=torch.as_tensor(weights, device=device).float())
-    optimiser = torch.optim.Adam(selfAttModel.parameters(), lr=config['learning_rate'])
 
     train(selfAttModel, criterion, optimiser, train_iterator, TEXT.vocab, device)
-    test(selfAttModel, test_iterator, TEXT.vocab)
 
     if config['save_model']:
         save_model(selfAttModel)
+
+    test(selfAttModel, test_iterator, TEXT.vocab)
